eb1d2522bacac8f14cb9f3fd0c175521e8db22cc
Michael Stewart <mstewart@mstewart-l.corp.instructure.com>
2018-10-19 13:45:17 -0600

    New update

--- functions.js
+++ functions.js
@@ -1,10 +1,16 @@
+//Logs the name when the value is given in the console
+
 function name(argument){
 	console.log("Hello! My name is " + argument + "! How are you doing today?");
 }
 
+//Multiplies the two values given
+
 function calc(number1, number2){
 	console.log(number1 + " times " + number2 + " equals " + number1 * number2)
 }
+
+//Prompts the user for their favorite city, then returns with a city capitalized in a string
 
 function capitalize(str){
 	return str.charAt(0).toUpperCase() + str.slice(1);
