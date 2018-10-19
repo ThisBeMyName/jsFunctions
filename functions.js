a8978cc6e7674d3402087b3b455e9f52ddd9bdf4
Michael Stewart <mstewart@mstewart-l.corp.instructure.com>
2018-10-19 09:13:15 -0600

    New files

---
+++ functions.js
@@ -0,0 +1,7 @@
+function name(user){
+	console.log("Hello! My name is " + user + "! How are you doing today?");
+}
+
+function calc(number1, number2){
+	console.log(number1 + " times " + number2 + " equals " + number1 * number2)
+}
